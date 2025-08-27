aa4cb1bf76781257aa459bd962e8f70d47d3efd6
Victor Matskiv <victormatskiv@Victors-MacBook-Pro-2.local>
2025-08-26 18:16:34 -0700

    Updated dev mode

--- src/naylence/agent/dev_mode.py
+++ src/naylence/agent/dev_mode.py
@@ -4,40 +4,40 @@ from typing import Any
 
 SENTINEL_PORT = 8000
 
-PROTO_MAJOR = 1
-BASE_WS_URL = f"ws://localhost:{SENTINEL_PORT}/fame/v{PROTO_MAJOR}/attach/ws/downstream"
-
-security_manager_config = {
-    "type": "DefaultNodeSecurityManager",
-    "security_policy": {
-        "type": "DefaultSecurityPolicy",
-        "signing": {
-            "signing_material": "raw-key",
-            # "require_cert_sid_match": True,
-        },
-    },
-}
-
-no_security_manager_config = {
-    "type": "NoSecurityManager",
-}
-
-enabled_security_manager_config = no_security_manager_config
 
 CLIENT_CONFIG = {
     "node": {
-        "mode": "dev",
-        "direct_parent_url": BASE_WS_URL,
-        "security": enabled_security_manager_config,
+        "security": {
+            "type": "SecurityProfile",
+            "profile": "${env:FAME_SECURITY_PROFILE:open}",
+        },
+        "admission": {
+            "type": "AdmissionProfile",
+            "profile": "${env:FAME_ADMISSION_PROFILE:open}",
+        },
+        "storage": {
+            "type": "StorageProfile",
+            "profile": "${env:STORAGE_PROFILE:memory}",
+        },
     }
 }
 
-AGENT_CONFIG = {
+NODE_CONFIG = {
     "node": {
-        "mode": "dev",
-        "direct_parent_url": BASE_WS_URL,
+        "type": "Node",
         "requested_logicals": ["fame.fabric"],
-        "security": enabled_security_manager_config,
+        "security": {
+            "type": "SecurityProfile",
+            "profile": "${env:FAME_SECURITY_PROFILE:open}",
+        },
+        "admission": {
+            "type": "AdmissionProfile",
+            "profile": "${env:FAME_ADMISSION_PROFILE:open}",
+        },
+        "storage": {
+            "type": "StorageProfile",
+            "profile": "${env:STORAGE_PROFILE:memory}",
+        },
     }
 }
 
@@ -54,10 +54,19 @@ SENTINEL_CONFIG = {
                 "port": SENTINEL_PORT,
             },
         ],
-        "mode": "dev",
         "requested_logicals": ["fame.fabric"],
-        "authorizer": {"type": "NoopAuthorizer"},
-        "security": enabled_security_manager_config,
+        "security": {
+            "type": "SecurityProfile",
+            "profile": "${env:FAME_SECURITY_PROFILE:open}",
+        },
+        "admission": {
+            "type": "AdmissionProfile",
+            "profile": "${env:FAME_ADMISSION_PROFILE:none}",
+        },
+        "storage": {
+            "type": "StorageProfile",
+            "profile": "${env:STORAGE_PROFILE:memory}",
+        },
     },
 }
 
@@ -86,24 +95,6 @@ def create_sentinel_app(*, config: Any = None, log_level: str | int = "info"):
     app.add_middleware(FameContextMiddleware)
 
     return app
-
-
-def run_sentinel_orig(
-    *,
-    config: Any = None,
-    port: int | None = None,
-    log_level: str | int = "info",
-    **kwargs,
-):
-    import os
-    import uvicorn
-
-    app = create_sentinel_app(config=config, log_level=log_level)
-
-    host = os.getenv("APP_HOST", "0.0.0.0")
-    port = port or int(os.getenv("APP_PORT", SENTINEL_PORT))
-
-    uvicorn.run(app, host=host, port=port, log_level="error")
 
 
 def run_sentinel(*, config: Any = None, log_level: str | int = "info", **kwargs):
